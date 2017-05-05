1420a366adedc47f992112700f67eb29a61fb858
Jeckel <jeckel@jeckel.fr>
2017-05-05 14:33:37 +0200

    Add test with Behat

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,56 +1,118 @@
 <?php
 
 use Behat\Behat\Context\Context;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
-use Behat\Behat\Tester\Exception\PendingException;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context
 {
+    const WORKING = 'working';
+
+    /**
+     * @var string
+     */
+    protected $base_url;
+    /**
+     * @var string
+     */
+    protected $mysql_file;
+    /**
+     * @var string
+     */
+    protected $redis_file;
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $client;
+    /**
+     * @var stdClass
+     */
+    protected $response;
+
+
     /**
      * Initializes context.
      *
      * Every scenario gets its own context instance.
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
+     * @param string $base_url
      */
-    public function __construct()
+    public function __construct(string $base_url)
     {
+        $this->base_url = $base_url;
+        $var_dir = dirname(__DIR__) . '/../var/';
+        $this->mysql_file = $var_dir . \AppBundle\Service\MysqlStatus::FILE_NAME;
+        $this->redis_file = $var_dir . \AppBundle\Service\RedisStatus::FILE_NAME;
+        $this->client      = new \GuzzleHttp\Client();
     }
 
     /**
-     * @Given there is :arg1, which is :arg2
+     * @BeforeScenario
      */
-    public function thereIsWhichIs($arg1, $arg2)
+    public function setup()
     {
-        throw new PendingException();
+        if (file_exists($this->redis_file)) {
+            unlink($this->redis_file);
+        }
+        if (file_exists($this->mysql_file)) {
+            unlink($this->mysql_file);
+        }
     }
 
     /**
-     * @Then Mysql status is :arg1
+     * @When I go to :url_arg
+     * @param $url_arg
      */
-    public function mysqlStatusIs($arg1)
+    public function iGoTo($url_arg)
     {
-        throw new PendingException();
+        $res = $this->client->get($this->base_url.$url_arg);
+        $this->response = json_decode($res->getBody()->__toString());
     }
 
     /**
-     * @Then Redis status is :arg1
+     * @Given there is :service, which is :status
+     * @param $service
+     * @param $status
      */
-    public function redisStatusIs($arg1)
+    public function thereIsWhichIs($service, $status)
     {
-        throw new PendingException();
+        if ($status != self::WORKING) {
+            return;
+        }
+        switch ($service) {
+            case "Mysql" :
+                touch($this->mysql_file);
+                break;
+            case "Redis" :
+                touch($this->redis_file);
+                break;
+        }
     }
 
     /**
-     * @Then App status is :arg1
+     * @Then :service status is :status
+     * @param $service
+     * @param $status
+     * @return bool
+     * @throws Exception
      */
-    public function appStatusIs($arg1)
+    public function serviceStatusIs($service, $status)
     {
-        throw new PendingException();
+        $service = strtoupper($service);
+        if (! is_object($this->response) || !isset($this->response->$service)) {
+            throw new Exception($service . " status not in response");
+        }
+        if ($status == self::WORKING) {
+            if ($this->response->$service !== true) {
+                throw new Exception($service . " expected to be working, but is not working : ".$this->response->$service);
+            }
+        } else {
+            if ($this->response->$service === true) {
+                throw new Exception($service . " expected to be not working, but it is working");
+            }
+        }
+        return false;
     }
-
 }
--- features/status.feature
+++ features/status.feature
@@ -1,10 +1,35 @@
 Feature: Status application
-
+  Test if the status service is answering correct value corresponding
+  to any dependent services (like MySQL and Redis)
 
   Scenario: MySQL and Redis are working
-    Given there is "Mysql", which is "running"
-    And there is "Redis", which is "running"
+    Given there is "Mysql", which is "working"
+    And there is "Redis", which is "working"
     When I go to "/status/"
-    Then Mysql status is "running"
-    And Redis status is "running"
-    And App status is "running"
+    Then Mysql status is "working"
+    And Redis status is "working"
+    And App status is "working"
+
+  Scenario: MySQL is working and Redis is not
+    Given there is "Mysql", which is "working"
+    And there is "Redis", which is "not working"
+    When I go to "/status/"
+    Then Mysql status is "working"
+    And Redis status is "not working"
+    And App status is "not working"
+
+  Scenario: MySQL is not working and Redis is
+    Given there is "Mysql", which is "not working"
+    And there is "Redis", which is "working"
+    When I go to "/status/"
+    Then Mysql status is "not working"
+    And Redis status is "working"
+    And App status is "not working"
+
+  Scenario: MySQL and Redis are not working
+    Given there is "Mysql", which is "not working"
+    And there is "Redis", which is "not working"
+    When I go to "/status/"
+    Then Mysql status is "not working"
+    And Redis status is "not working"
+    And App status is "not working"
